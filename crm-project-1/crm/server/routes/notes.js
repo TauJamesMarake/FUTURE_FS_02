23fc64024d342c14d6c7170b49b7bf2516972491
Tau Marake <jtaumarake@gmail.com>
2026-03-23 16:19:35 +0200

    Delete crm-project-1/crm directory

--- crm-project-1/crm/server/routes/notes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router({ mergeParams: true }); // inherit :id from parent
-const { getNotes, addNote } = require('../controllers/notesController');
-const { protect }           = require('../middleware/auth');
-
-router.get('/',  protect, getNotes);
-router.post('/', protect, addNote);
-
-module.exports = router;
